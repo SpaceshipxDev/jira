eb2df8e48772696df6ed72aa3ed1cd25efbcbf4a
SpaceshipxDev <150497663+SpaceshipxDev@users.noreply.github.com>
2025-08-15 20:18:03 +0800

    Add editable spreadsheet page

---
+++ app/spreadsheet/page.tsx
@@ -0,0 +1,9 @@
+import EditableSpreadsheet from '@/components/EditableSpreadsheet';
+
+export default function SpreadsheetPage() {
+  return (
+    <main className="p-4">
+      <EditableSpreadsheet />
+    </main>
+  );
+}
